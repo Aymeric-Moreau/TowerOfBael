aa09fed4323c335646d9b4cfc47e54747ae81a2e
Houcine-Hachemaoui <houcine.ali.hachemaoui@gmail.com>
2025-12-24 20:06:15 +0100

    larme par default fini avec animation !

--- Assets/Script/Houcine/Character/Tear_Character.cs
+++ Assets/Script/Houcine/Character/Tear_Character.cs
@@ -10,6 +10,34 @@ public class Tear_Character : MonoBehaviour
     public Vector2 direction; // Direction dans laquelle la larme par défault  se déplace
     [HideInInspector] public float tear_damage; // Dégâts de larme par défault (gérée depuis le player)
 
+
+    void Start()
+    {
+        // Afficher la bonne orientaion de la larme par défault
+        // On récupère le transform pour rotation
+        Vector3 rotation = Vector3.zero;
+
+        // Permet de déplacer la larme à vitesse constante dans toutes les directions
+        direction = direction.normalized;
+
+
+        // Tir vertical
+        if (direction.y > 0 && direction.x == 0) rotation.z = 180;      // Haut
+        else if (direction.y < 0 && direction.x == 0) rotation.z = 0; // Bas
+
+        // Tir horizontal
+        else if (direction.x > 0 && direction.y == 0) rotation.z = -270; // Droite
+        else if (direction.x < 0 && direction.y == 0) rotation.z = -90;  // Gauche
+
+        // Diagonales
+        else if (direction.x > 0 && direction.y > 0) rotation.z = 135;   // Haut-droite
+        else if (direction.x < 0 && direction.y > 0) rotation.z = 225;    // Haut-gauche
+        else if (direction.x > 0 && direction.y < 0) rotation.z = 45;   // Bas-droite
+        else if (direction.x < 0 && direction.y < 0) rotation.z = -45;   // Bas-gauche
+
+        // Applique la rotation
+        transform.rotation = Quaternion.Euler(rotation);
+    }
     void Update()
     {
         // Déplace la larme dans la direction spécifiée à la vitesse donnée
