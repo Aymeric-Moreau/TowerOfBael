f2f1c27496463a4d73769ed77bb18ecbccb85417
aymeric-moreau <aymeric.moreau13@proton.me>
2025-12-28 17:48:39 +0100

    update col room + fuyeur

--- Assets/Script/Aymeric/AIEnnemisFuyeur.cs
+++ Assets/Script/Aymeric/AIEnnemisFuyeur.cs
@@ -13,6 +13,13 @@ public class AIEnnemisFuyeur : MonoBehaviour
     private Animator anim;
     private bool IsMovingAnim;
 
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -22,25 +29,50 @@ public class AIEnnemisFuyeur : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (target == null) return;
+        //if (target.position.x - transform.position.x < 7 && target.position.y - transform.position.y < 7)
+        //{
+        //    Vector3 direction = new Vector3(target.position.x - transform.position.x,target.position.y - transform.position.y,transform.position.z);
+        //    direction.Normalize();
+
+        //    //transform.position -= direction * speed * Time.deltaTime;
+        //    rb.velocity = direction * speed;
+
+        //    // L’ennemi bouge → Walk
+        //    IsMovingAnim = true;
+        //}
+        //else
+        //{
+        //    // Ennemi trop loin → Idle
+        //    IsMovingAnim = false;
+        //}
+        //// On applique au Animator
+        //anim.SetBool("IsMoving", IsMovingAnim);
+
+
+    }
+
+    private void FixedUpdate()
+    {
         if (target == null) return;
         if (target.position.x - transform.position.x < 7 && target.position.y - transform.position.y < 7)
         {
-            Vector3 direction = new Vector3(target.position.x - transform.position.x,target.position.y - transform.position.y,transform.position.z);
+            Vector3 direction = new Vector3(target.position.x - transform.position.x, target.position.y - transform.position.y, transform.position.z);
             direction.Normalize();
 
-            transform.position -= direction * speed * Time.deltaTime;
+            //transform.position -= direction * speed * Time.deltaTime;
+            rb.velocity = - direction * speed;
 
             // L’ennemi bouge → Walk
             IsMovingAnim = true;
         }
         else
         {
+            rb.velocity = Vector3.zero;
             // Ennemi trop loin → Idle
             IsMovingAnim = false;
         }
         // On applique au Animator
         anim.SetBool("IsMoving", IsMovingAnim);
-
-
     }
 }
