4145b5cef215650e99c81f6b42c823095fac4895
aymeric-moreau <aymeric.moreau13@proton.me>
2026-01-28 23:57:31 +0100

    début update placement des room

--- Assets/Script/Aymeric/GenerationProceduralManager.cs
+++ Assets/Script/Aymeric/GenerationProceduralManager.cs
@@ -501,7 +501,9 @@ public class GenerationProceduralManager : MonoBehaviour
             GameObject room = prefabSalle[Array.IndexOf(typeSalleParIndex, type)];
 
 
-            Vector3 posRoom = new Vector3(index.x * room.transform.localScale.x * 1.4f, index.y * room.transform.localScale.y * 1.5f, 0);
+            //Vector3 posRoom = new Vector3(index.x * room.transform.localScale.x +20 * 1.4f, index.y * room.transform.localScale.y +30 * 1.5f, 0);
+            Vector3 posRoom = new Vector3(index.x * (room.transform.localScale.x +20) * 1.4f, index.y * (room.transform.localScale.y +30) * 1.5f, 0);
+            Debug.Log("type : "+type.ToString() + "position " + posRoom + " index" + index.x+ "." + index.y + "room scal calcule" + room.transform.localScale.x  * 1.4f);
             GameObject roomIntstance = Instantiate(room, posRoom, Quaternion.identity);
             RoomManager RMScript = roomIntstance.GetComponent<RoomManager>();
             SpawnObstacle(type, posRoom, salle, RMScript);
